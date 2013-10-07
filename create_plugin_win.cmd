c1f262fe377a1e376dd3e291d807dd636c4d86d8
Pol Monso Purti <polmonso@epfl.ch>
2013-10-07 20:03:46 +0200

    modification of create_plugin_win.cmd to add the cuda and nppd libraries

--- create_plugin_win.cmd
+++ create_plugin_win.cmd
@@ -4,4 +4,4 @@ javac -cp C:\Users\monso\code\fiji64\Fiji.app\jars\*;.;lib -d ../bin ch\epfl\cvl
 
 cd ..
 
-jar -cvf finalPlugin\SSVM_Plugin.jar plugins.config -C bin ch -C bin train.exe -C bin predict.exe 
+jar -cvf finalPlugin\SSVM_Plugin.jar plugins.config -C bin ch -C bin train.exe -C bin predict.exe -C bin cudart64_50_35.dll -C bin npp64_50_35.dll 
